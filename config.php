78d472b7e46bf60dacc7ebe674ccdb295dcc1914
Aaron Pop <FezFamiliar@yahoo.com>
2019-12-03 03:46:40 -0800

    cleaned soe more

--- config.php
+++ config.php
@@ -13,6 +13,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
      <meta charset="utf-8">
      <title>Supermarket</title>
      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
      <link rel="stylesheet" href="style.css">
      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
@@ -22,7 +23,6 @@ $root = $_SERVER['DOCUMENT_ROOT'];
   integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
   crossorigin="anonymous"></script>
    <script type="text/javascript" src="functions/functions.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
    </head>
    <body>
--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 
 include 'config.php';
 
-if(isset($_GET['cart'])) header( "refresh:0;url=".str_replace('&cart=true','',$_SERVER['REQUEST_URI']));
-if(isset($_GET['payed'])){
-
- foreach ($_SESSION['cart'] as $key => $products) {
-       if($key == 'total')continue;
-        mysqli_query($conn,"UPDATE `products` SET stock = stock - '".$products['quantity']."' WHERE id = '". $products['id']."'");
- }
-
- unset($_SESSION['cart']);
- header("refresh:0;url=index.php");
-}
 //print_array($_SESSION);
 
 if(isset($_GET['logout'])){
@@ -22,22 +11,45 @@ if(isset($_GET['logout'])){
 }
 include 'header.php';
 $page = 3;
+if(isset($_GET['add']) && $_GET['add'] == 'true'): ?>
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+          <form action="" method="POST" autocomplete="off" class="comment-form">
+          <br>
+          Titlu:
+          <input type="text" name="titlu" class="form-control">
+          <br>
+          Mesaj:
+          <textarea class="form-control" rows="3" name="msg">
+            
 
+          </textarea>
+          <br>
+          <input type="submit" value="Salveaza" class="btn btn-primary submit">
+        </form>
 
-  $_SESSION['cart'][$_SESSION['cart']['total']] = array(
+          
 
-    'id' => trim($_POST['id']),
-    'name' => trim($_POST['name']),
-    'description' =>  trim($_POST['description']),
-    'price' => trim($_POST['price']),
-    'quantity' => trim($_POST['quantity'])
+<? 
 
-  );
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
+    $cat_id = $_GET['category_id'];
+    $title = $_POST['titlu'];
+    $msg = $_POST['msg'];
+    /*   echo "INSERT INTO `posts`(post_id,category_id,title,message,posted) VALUES('1' ,'". $cat_id."', '".$title."' ,'".$msg."', NOW())";*/
 
-}
+    $query =  mysqli_query($conn,"INSERT INTO `posts`(post_id,category_id,title,message,posted) VALUES('1' ,'". $cat_id."', '".$title."' ,'".$msg."', NOW())");
+
+    if($query){
+      echo 'comentul dvs a fost postat!';
+    }
+    else{    echo 'eroare!';
+
+ }
+
+  }
+
+endif; 
 
 if(isset($_GET['category_id'])){
 
@@ -52,23 +64,27 @@ if(isset($_GET['category_id'])){
 include 'menu.php';
 
    if(isset($_GET['category_id'])): ?>
+     <a href="<?= $_SERVER['REQUEST_URI']; ?>&add=true" class="addpost">Adaugati o postare!</a>
        <section class="container">
            <? 
            if($rowcount > 0):
                  while($row = mysqli_fetch_array($get_posts)): ?>
-                <div class="products">
+                <div class="posts">
 
-
-             </div>
+                  <p><?= $row['title']; ?></p>
+                  <p><?= $row['message']; ?></p>
+                  <p><?= $row['posted']; ?></p>
+                </div> 
            <? endwhile;else: echo "Nu aveti nicio postare!";endif;?>
-              <a href="?category_id=<?= $_GET['category_id']; ?>&page=<?= (($_GET['page'] + 1) % ($_SESSION['totalpage']+1) == 0) ? 1 : $_GET['page'] + 1; ?>">
+           
+  <!--             <a href="?category_id=<?= $_GET['category_id']; ?>&page=<?= (($_GET['page'] + 1) % ($_SESSION['totalpage']+1) == 0) ? 1 : $_GET['page'] + 1; ?>">
                 <img src="lightbox/images/next.png" class="next">
               </a>
             <div class="pages">
                 <? for($i = 1; $i <= $_SESSION['totalpage']; $i++): ?>
                   <a href="?category_id=<?= $_GET['category_id']; ?>&page=<?= $i; ?>" class="<? if($_GET['page'] == $i) echo 'selected'; ?>"><?= $i; ?></a>
                 <? endfor; ?>
-            </div>
+            </div> -->
        </section>
 
 <? endif;?>
